b82ae82587a50c9936510de8c86aa1f9e33363e4
guk <gukiq@outlook.com>
2025-12-15 11:56:54 +0800

    去掉connectivity_plus

--- lib/src/network_connectivity.dart
+++ lib/src/network_connectivity.dart
@@ -1,9 +1,7 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-
+// import 'package:connectivity_plus/connectivity_plus.dart';
 
 /// Checking if the device is connected to the internet.
 class NetworkConnectivity {
-
   static final NetworkConnectivity _singleton = NetworkConnectivity._internal();
 
   factory NetworkConnectivity() {
@@ -13,12 +11,13 @@ class NetworkConnectivity {
   NetworkConnectivity._internal();
 
   /// Checking if the device is connected to the internet.
-   Future<bool> get connected async {
-    var connectivityResult = await (Connectivity().checkConnectivity());
-    if (connectivityResult == ConnectivityResult.none) {
-      return false;
-    } else {
-      return true;
-    }
+  Future<bool> get connected async {
+    // var connectivityResult = await (Connectivity().checkConnectivity());
+    // if (connectivityResult == ConnectivityResult.none) {
+    //   return false;
+    // } else {
+    //   return true;
+    // }
+    return true;
   }
 }
